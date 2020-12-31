1df64d17d130c4e55090b0d2c147440769d5a96a
Yong Jheng Tang <tyj@yongs-mbp.home>
2020-12-31 22:37:25 +0000

    remove unused router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 import './App.scss';
+
+import CheckEligibility from 'pages/CheckEligibility';
+import EligibleCardList from 'pages/CardList';
+import CardDetails from 'pages/CardDetails';
 
 import TotallyMoneyRouter from './router';
 
 function App() {
-  return <TotallyMoneyRouter />;
+  return (
+    <Router>
+      <Switch>
+        <Redirect exact from="/" to="/check_eligibility" />
+
+        <Route exact path="/check_eligibility" component={CheckEligibility} />
+
+        <Route
+          exact
+          path="/cardlist/employee_status=:status&income_range=:income"
+          component={EligibleCardList}
+        />
+
+        <Route path="/card_details/:card_type" component={CardDetails} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
